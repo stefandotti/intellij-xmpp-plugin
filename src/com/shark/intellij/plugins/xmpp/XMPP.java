1626b8aad4074bfc2ef2011e9ed25fd6f67a9e2d
dos <stefan.dotti@shark-soft.com>
2016-11-17 10:43:27 +0100

    changed property check

--- src/com/shark/intellij/plugins/xmpp/XMPP.java
+++
@@ -1,9 +0,0 @@
-package com.shark.intellij.plugins.xmpp;
-
-public class XMPP {
-
-    public enum STATE {
-        ACTIVE, INACTIVE;
-    }
-
-}
--- src/com/shark/intellij/plugins/xmpp/XMPPProjectInterface.java
+++
@@ -1,4 +0,0 @@
-package com.shark.intellij.plugins.xmpp;
-
-public interface XMPPProjectInterface {
-}

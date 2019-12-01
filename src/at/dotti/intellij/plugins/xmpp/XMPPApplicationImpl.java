645f897cc0511cfc407a7196c793e1aa53f28182
Stefan Dotti <stefan@dotti.at>
2019-12-01 20:13:39 +0100

    removed deprecated api

--- src/at/dotti/intellij/plugins/xmpp/XMPPApplicationImpl.java
+++ src/at/dotti/intellij/plugins/xmpp/XMPPApplicationImpl.java
@@ -1,32 +1,14 @@
 package at.dotti.intellij.plugins.xmpp;
 
-import com.intellij.openapi.components.ApplicationComponent;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.Disposable;
 
-public class XMPPApplicationImpl implements XMPPApplicationInterface, ApplicationComponent {
-    @Override
-    public void initComponent() {
-        /*try {
-            XMPPService.create();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (XMPPException e) {
-            e.printStackTrace();
-        } catch (SmackException e) {
-            e.printStackTrace();
-        }*/
-    }
+public class XMPPApplicationImpl implements XMPPApplicationInterface, Disposable {
 
     @Override
-    public void disposeComponent() {
+    public void dispose() {
         if (XMPPService.getService() != null) {
             XMPPService.getService().close();
         }
     }
 
-    @NotNull
-    @Override
-    public String getComponentName() {
-        return XMPP.class.getSimpleName();
-    }
 }
